985171c0f3fa2f9c7a7132edec067ac7c44736d0
khyzhniak <khyzhniakihor@gmail.com>
2017-10-11 06:46:49 +0300

    Add files via upload

--- lendingQuas/js/common.js
+++ lendingQuas/js/common.js
@@ -1,15 +1,1 @@
-
- $(document).ready(function() {
-     $(".modalbox").fancybox();
-     $("#f_contact").submit(function() {
-         return false; });
-     $("#f_send").on("click", function() {
-
-         // тут дальнейшие действия по обработке формы
-         // закрываем окно, как правило делать это нужно после обработки данных
-         $("#f_contact").fadeOut("fast", function() {
-             $(this).before("<p><strong>Ваше сообщение отправлено!</strong></p>");
-             setTimeout("$.fancybox.close()", 1000);
-         });
-     });
- });
+$(document).ready(function(){$(".modalbox").fancybox();$("#f_contact").submit(function(){return false;});$("#f_send").on("click",function(){$("#f_contact").fadeOut("fast",function(){$(this).before("<p><strong>Ваше сообщение отправлено!</strong></p>");setTimeout("$.fancybox.close()",1000);});});});
