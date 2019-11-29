deaca0d3e795f0c82aa69290971e3b1eaf210ca2
khyzhniak <khyzhniakihor@gmail.com>
2019-11-29 15:32:32 +0200

    delete

--- Instant/js/main.js
+++ Instant/js/main.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function( $ ) {
-  
+    $('.sl_portfolio_page').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+    $("a.group").fancybox();
 });
